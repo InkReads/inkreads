be9583a7988d53562697de7a030072f57f1cb846
Ethan Jacinto <ethankylejacinto@gmail.com>
2025-03-03 15:04:03 -0800

    fix: formatting / component names

--- src/modules/components/landing/landing-hero/index.tsx
+++ src/modules/components/landing/landing-hero/index.tsx
@@ -14,9 +14,9 @@ export default function Hero() {
     >
       <LandingHeader />
       <Image src={BGImage} alt="bg-image" quality={100} className="absolute top-0 -z-10 brightness-[0.5] h-full w-full" />
-      <div className="max-w-5xl flex justify-evenly items-center gap-48">
+      <div className="max-w-5xl flex justify-evenly items-center gap-40">
         <HeroIntro />
-        <Image src={HeroImage} alt="hero-image" height={750} className="rounded-xl object-cover hidden lg:inline-block"/>
+        <Image src={HeroImage} alt="hero-image" height={450} className="rounded-xl object-cover hidden lg:inline-block"/>
       </div>
     </section>
   );
