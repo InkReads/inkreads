f1df2e8efefe9dda446a8dbfda494be9a3f8ead3
Ethan <ethankylejacinto@gmail.com>
2025-04-23 20:47:51 -0700

    feat: landing page styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,50 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from '@/pages/auth/Login';
-import Signup from '@/pages/auth/Signup';
-import Home from '@/pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AuthCheck from './components/auth/AuthCheck';
+import Login from './pages/auth/Login';
+import Signup from './pages/auth/Signup';
+import Home from './pages/Home';
+import Landing from './pages/Landing';
 
-function App() {
+export default function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  )
-}
+        {/* Public routes */}
+        <Route
+          path="/landing"
+          element={
+            <AuthCheck requireAuth={false}>
+              <Landing />
+            </AuthCheck>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <AuthCheck requireAuth={false}>
+              <Login />
+            </AuthCheck>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <AuthCheck requireAuth={false}>
+              <Signup />
+            </AuthCheck>
+          }
+        />
 
-export default App
+        {/* Protected routes */}
+        <Route
+          path="/"
+          element={
+            <AuthCheck>
+              <Home />
+            </AuthCheck>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
